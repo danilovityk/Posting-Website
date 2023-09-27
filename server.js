16acf88582344ad8d585d20196fc653fca3f2876
Danylo <danilovityk@gmail.com>
2023-09-27 15:07:12 -0400

    Finished the assigment

--- server.js
+++ server.js
@@ -1,15 +1,16 @@
 const serv = require('./blog-service')
+const path = require('path');
 const express = require('express'); // "require" the Express module
 const app = express(); // obtain the "app" object
 const HTTP_PORT = process.env.PORT || 8080; // assign a port
 
 // start the server on the port and output a confirmation ot the console
 serv.initialize()
-    .then(() =>
-    {
-        app.listen(HTTP_PORT, () => console.log(`server listening on: ${HTTP_PORT}`))
-    }).catch((err)=>{
-    console.log(err)
+.then(() =>
+{
+    app.listen(HTTP_PORT, () => console.log(`server listening on: ${HTTP_PORT}`))
+}).catch((err)=>{
+    console.log('Initialization error')
     })
 
 app.get('/', (req, res) => {
@@ -17,20 +18,29 @@ app.get('/', (req, res) => {
 });
   
 app.get('/about', (req, res) => {
-    res.sendFile('/Users/danilovityk/Desktop/web322-app/views/about.html');
+    res.sendFile(path.join(__dirname, '/views/about.html'));
 });
 
 app.get('/blog', (req, res) =>
 {
-    serv.getPublishedPosts().then((data) =>{res.json(data)}).catch((err) => res.send({"message": err}))
+    serv.getPublishedPosts()
+        .then((data) => { res.send(data) })
+        .catch((err) => res.send({ "message": err }))
 });
   
 app.get('/posts', (req, res) =>
 {
-    serv.getAllPosts().then((data) => { res.json(JSON.stringify(data)) }).catch((err) => res.send({"message": err}))
+    serv.getAllPosts()
+        .then((data) => res.send(data))
+        .catch((err) => res.send({ "message:": err }))
 });
   
 app.get('/categories', (req, res) => {
-    serv.getCategories().then((data) => {res.json(data)}).catch((err) => res.send({"message": err}))
+    serv.getCategories()
+        .then((data) => { res.send(data) })
+        .catch((err) => res.send({ "message": err }))
 });
   
+app.use((req, res) => {
+    res.status(404).sendFile(path.join(__dirname, '/views/404.html'));
+  });
