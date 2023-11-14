8e6f249bd10fd3d49902c6aeb531884e8eb31138
Danylo <danilovityk@gmail.com>
2023-11-14 10:11:59 -0500

    deleted json files, helper added

--- blog-service.js
+++ blog-service.js
@@ -1,188 +1,87 @@
-const fs = require('fs')
-const blogData = require("./blog-service");
-var posts = [];
-var categories = [];
-
+const Sequelize = require('sequelize');
+var sequelize = new Sequelize('SenecaDB', 'danilovityk', '4Nrow0nclMzU', {
+    host: 'ep-steep-base-90367442-pooler.us-east-1.aws.neon.tech',
+    dialect: 'postgres',
+    port: 5432,
+    dialectOptions: {
+        ssl: { rejectUnauthorized: false }
+    },
+    query: { raw: true }
+});
 
 
 async function initialize()
 {
     // Get the category list from server.
    
-    return new Promise((resolve, reject) =>
-    {
-        postsRead();
-        function postsRead()
-        {
-            fs.readFile('./data/posts.json', 'utf8', (err, data) =>
-            {
-                if (err) reject(err);
-                posts = JSON.parse(data);
-                categoriesRead();
-            }
-            );
-       } 
-        function categoriesRead()
-        {
-            fs.readFile('./data/categories.json', 'utf8', (err, data) =>
-            {
-                if (err) reject(err);
-                categories = JSON.parse(data);   
-                resolve('Operation success');
-            }
-            );
-          
-        }
+    return new Promise((resolve, reject) => {
+        reject();
     });
 
-
-    
 }
 
 async function getAllPosts()
 {
-    return new Promise((resolve, reject) =>
-    {
-        if (posts.length != 0)
-        {
-            resolve(posts);
-        } else
-        {
-            reject('No results returned')
-        }
+    return new Promise((resolve, reject) => {
+        reject();
+    });
 
-    })
 }
 
-async function getPublishedPostsByCategory(category){
-    return new Promise((resolve, reject) =>
-    {
-        if (posts.length != 0)
-        {
-            var publishedArray = [];
-            posts.forEach(post => {
-                if (post.published == true && post.category == category)
-                {
-                    publishedArray.push(post);
-                }
-            });
-            resolve(publishedArray);
-        } else
-        {
-            reject('No results returned')
-        }
+async function getPublishedPostsByCategory(category)
+{
+    return new Promise((resolve, reject) => {
+        reject();
+    });
 
-    })
 }
 
 async function getPublishedPosts()
 {
-    return new Promise((resolve, reject) =>
-    {
-        if (posts.length != 0)
-        {
-            var publishedArray = [];
-            posts.forEach(post => {
-                if (post.published == true)
-                {
-                    publishedArray.push(post);
-                }
-            });
-            resolve(publishedArray);
-        } else
-        {
-            reject('No results returned')
-        }
+    return new Promise((resolve, reject) => {
+        reject();
+    });
 
-    })
 }
 
 async function getCategories()
 {
-    return new Promise((resolve, reject) =>
-    {
-        if (categories.length != 0)
-        {
-            resolve(categories);
-        } else
-        {
-            reject('No results returned')
-        }
-    })
+    return new Promise((resolve, reject) => {
+        reject();
+    });
+
 }
 
 function addPost(postData)
 {
-    return new Promise((resolve, reject) =>
-    {
-        if (!postData.published)
-        {
-            postData.published = false;
-        } else
-        {
-            postData.published = true;
-        }
+    return new Promise((resolve, reject) => {
+        reject();
+    });
 
-        postData.id = posts.length + 1;
-        var currentDate = new Date()
-        postData.postDate = `${currentDate.getFullYear()}-${currentDate.getMonth() + 1}-${currentDate.getDate()}` 
-        posts.push(postData)
-
-        resolve(postData)
-    })
 }
 
 function getPostsByCategory(category)
 {
-    return new Promise((resolve, reject) =>
-    {
-        var categorizedArray = []
-        posts.forEach((post) =>
-        {
-            if (post.category == category) categorizedArray.push(post);
-        })
-        if (categorizedArray.length != 0)
-        {
-            resolve(categorizedArray);
-        } else
-        {
-            reject("No Results Returned");
-        }
+    return new Promise((resolve, reject) => {
+        reject();
     });
+
 }
 
 function getPostsByMinDate(minDateStr)
 {
-        return new Promise((resolve, reject) =>
-        {
-            var filteredArray = []
-            posts.forEach((post) =>
-            {
-                if (new Date(post.postDate) >= new Date(minDateStr)) filteredArray.push(post);
-                
-            });
-            if (filteredArray.length != 0)
-            {
-                resolve(filteredArray);
-            } else
-            {
-                reject("No Results Returned");
-            }
-        })
+    return new Promise((resolve, reject) => {
+        reject();
+    });
+
 }
 
 function getPostById(id)
 {
-    return new Promise((resolve, reject) =>
-    {
-        posts.forEach(post =>
-        {
-            if (post.id == id) resolve(post)
-        })
-            
-        
-        reject("No object with given id found");
-    })
+    return new Promise((resolve, reject) => {
+        reject();
+    });
+
 }
 
 
