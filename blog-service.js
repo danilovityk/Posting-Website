a9dc9e0d499ca7795b870e02559bdc906e9e91ef
Danylo <danilovityk@gmail.com>
2023-11-14 12:19:05 -0500

    added sql querriest to blog service

--- blog-service.js
+++ blog-service.js
@@ -9,13 +9,29 @@ var sequelize = new Sequelize('SenecaDB', 'danilovityk', '4Nrow0nclMzU', {
     query: { raw: true }
 });
 
+const Post = sequelize.define('Post', {
+    body: Sequelize.TEXT,
+    title: Sequelize.STRING,
+    postDate: Sequelize.DATE,
+    featureImage: Sequelize.STRING,
+    published: Sequelize.BOOLEAN
+});
+  
+const Category = sequelize.define('Post', {
+    category: Sequelize.STRING
+});
+
+Post.belongsTo(Category, {foreignKey: 'category'});
+
 
 async function initialize()
 {
-    // Get the category list from server.
-   
-    return new Promise((resolve, reject) => {
-        reject();
+    return new Promise((resolve, reject) =>
+    {
+        sequelize.sync().then((data) =>
+        {
+            resolve(data);
+        }).catch(err => reject('unable to sync the database'));
     });
 
 }
@@ -23,15 +39,20 @@ async function initialize()
 async function getAllPosts()
 {
     return new Promise((resolve, reject) => {
-        reject();
+        Post.findAll().then(data => resolve(data))
+            .catch(err => reject('no results retuned'));
     });
-
 }
 
 async function getPublishedPostsByCategory(category)
 {
     return new Promise((resolve, reject) => {
-        reject();
+        Post.findAll({
+            where: {
+                published: true,
+                category: category
+            }
+        }).then(data => resolve(data)).catch(err => reject('no results returned'))
     });
 
 }
@@ -39,7 +60,11 @@ async function getPublishedPostsByCategory(category)
 async function getPublishedPosts()
 {
     return new Promise((resolve, reject) => {
-        reject();
+        Post.getAll({
+            where: {
+                published: true,
+            }
+        }).then(data => resolve(data)).catch(err => reject('no results returned'))
     });
 
 }
@@ -47,7 +72,10 @@ async function getPublishedPosts()
 async function getCategories()
 {
     return new Promise((resolve, reject) => {
-        reject();
+        Category.findAll().then(data =>
+        {
+           resolve(data)
+       }).catch(err => reject('no results returned'))
     });
 
 }
@@ -55,7 +83,22 @@ async function getCategories()
 function addPost(postData)
 {
     return new Promise((resolve, reject) => {
-        reject();
+        postData.published = (postData.published) ? true : false;
+        for (prop in postData)
+        {
+            if (prop == "")
+            {
+                prop = null;
+            }
+        }
+        postData.postDate = new Date();
+        Post.create({
+            title: postData.title,
+            body: postData.body,
+            postDate: postData.postDate,
+            featureImage: postData.featureImage,
+            published: postData.published
+        }).then(resolve()).catch(err => reject('could not create an abject'))
     });
 
 }
@@ -63,7 +106,13 @@ function addPost(postData)
 function getPostsByCategory(category)
 {
     return new Promise((resolve, reject) => {
-        reject();
+        Post.findAll({
+            where: {
+                category: category
+        }}).then((data) =>
+        {
+           resolve(data)
+        }).catch(err => reject('no results returned'));
     });
 
 }
@@ -71,7 +120,14 @@ function getPostsByCategory(category)
 function getPostsByMinDate(minDateStr)
 {
     return new Promise((resolve, reject) => {
-        reject();
+        Post.findAll({
+            where: {
+                postDate: { $gte: new Date(minDateStr) }
+            }
+        }).then(data =>
+        {
+            
+        }).catch(err => reject('No results returned'))
     });
 
 }
@@ -79,10 +135,17 @@ function getPostsByMinDate(minDateStr)
 function getPostById(id)
 {
     return new Promise((resolve, reject) => {
-        reject();
+        Post.findAll({
+            where: {
+            id: id
+        }}).then(data =>
+        {
+            resolve(data[0])     
+        }).catch (err => reject('no results returned'))
     });
 
 }
+
 
 
 module.exports = { initialize, getAllPosts, getPublishedPosts, getCategories, addPost, getPostsByCategory, getPostsByMinDate, getPostById, getPublishedPostsByCategory};
